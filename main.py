cfc2fb2b549db8bfaa594a584dbe507eb10a154b
pere49 <perekevin49@gmail.com>
2024-05-16 15:57:24 +0300

    delete operation

--- main.py
+++ main.py
@@ -72,6 +72,11 @@ def find_post(id):
     for post in my_posts:
         if post["id"] == id:
             return post
+        
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p["id"] == id:
+            return i
 
 @app.get("/")
 async def root():
@@ -101,3 +106,13 @@ def get_post(id: int, response: Response):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id {id} was not found")
     return {"post-detail": find_post(id)}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Post with id {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
