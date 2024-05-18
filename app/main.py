2cbab0fcce4ba5e0dfc63c4937ce41a75b4958c4
pere49 <perekevin49@gmail.com>
2024-05-18 15:59:49 +0300

    Connecting to Postgres and performing CRUD operations

--- app/main.py
+++ app/main.py
@@ -3,7 +3,13 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # fastapi instance
 app = FastAPI()
 
@@ -15,59 +21,37 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+""" Connecting to PostGres DB """
+HOST=os.getenv('HOST')
+DATABASE_NAME=os.getenv('DATABASE_NAME')
+USER=os.getenv('USER')
+PASSWORD=os.getenv('PASSWORD')
+while True:
+  try:
+      conn = psycopg2.connect(host=HOST, database=DATABASE_NAME, user=USER, password=PASSWORD, cursor_factory=RealDictCursor)
+      cursor = conn.cursor()
+      print("Database connection was successful")
+      break
+  except Exception as error:
+      print("Connection to database failed")
+      print(f"error : {error}")
+      time.sleep(2)
+
+
 my_posts = [
-  {
-    "title": "Amazing Clouds",
-    "content": "Wispy formations danced across the cerulean canvas.",
-    "id": 1
-  },
-  {
-    "title": "Cat Wisdom",
-    "content": "The purrfect nap spot is always a sunbeam.",
-    "id": 2
-  },
-  {
-    "title": "Baking Bonanza",
-    "content": "The aroma of freshly baked cookies filled the air with delight.",
-    "id": 3
-  },
-  {
-    "title": "Underwater Adventure",
-    "content": "Schools of colorful fish darted through the coral reef.",
-    "id": 4
-  },
-  {
-    "title": "Power of Music",
-    "content": "Melodies that could soothe the soul and ignite the spirit.",
-    "id": 5
-  },
-  {
-    "title": "Starry Night",
-    "content": "A blanket of twinkling diamonds adorned the inky expanse.",
-    "id": 6
-  },
-  {
-    "title": "City Lights",
-    "content": "A symphony of neon signs illuminated the bustling metropolis.",
-    "id": 7
-  },
-  {
-    "title": "Forest Symphony",
-    "content": "The rustle of leaves and the chirping of birds created a harmonious chorus.",
-    "id": 8
-  },
-  {
-    "title": "Bookworm Bliss",
-    "content": "Getting lost in a captivating story, transported to another world.",
-    "id": 9
-  },
-  {
-    "title": "Rainy Day Dreams",    
-    "content": "The rhythmic pitter-patter on the windowpane, a soothing lullaby.",
-    "id": 10
-  }
+  {"title": "Amazing Clouds","content": "Wispy formations danced across the cerulean canvas.","id": 1},
+  {"title": "Cat Wisdom","content": "The purrfect nap spot is always a sunbeam.","id": 2  },
+  {"title": "Baking Bonanza","content": "The aroma of freshly baked cookies filled the air with delight.","id": 3  },
+  {"title": "Underwater Adventure","content": "Schools of colorful fish darted through the coral reef.","id": 4  },
+  {"title": "Power of Music","content": "Melodies that could soothe the soul and ignite the spirit.","id": 5  },
+  {"title": "Starry Night","content": "A blanket of twinkling diamonds adorned the inky expanse.","id": 6  },
+  {"title": "City Lights","content": "A symphony of neon signs illuminated the bustling metropolis.","id": 7  },
+  {"title": "Forest Symphony","content": "The rustle of leaves and the chirping of birds created a harmonious chorus.","id": 8  },
+  {"title": "Bookworm Bliss","content": "Getting lost in a captivating story, transported to another world.","id": 9  },
+  {"title": "Rainy Day Dreams",    "content": "The rhythmic pitter-patter on the windowpane, a soothing lullaby.","id": 10  }
 ]
 
+""" Utility functions """
 def find_post(id):
     for post in my_posts:
         if post["id"] == id:
@@ -78,25 +62,43 @@ def find_index_post(id):
         if p["id"] == id:
             return i
 
+
+""" API Routes """
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute(""" SELECT * FROM posts; """)
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.model_dump()
-    post_dict["id"] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    # Method 1 using local data
+    # post_dict = post.model_dump()
+    # post_dict["id"] = randrange(0, 1000000)
+    # my_posts.append(post_dict)
+
+    # Method 2 using postgres database
+    cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+
+    return {"data": new_post}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-    post = find_post(id)
+    # Method 1: using local data
+    # post = find_post(id)
+
+    # Method 2: Using postgres ql
+    cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id)))
+    post = cursor.fetchone()
     if not post:
         # method 1
         # response.status_code = status.HTTP_404_NOT_FOUND
@@ -105,30 +107,45 @@ def get_post(id: int, response: Response):
         # method 2
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id {id} was not found")
-    return {"post-detail": find_post(id)}
+    return {"post-detail": post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
+    # Method 1: Using local data
     index = find_index_post(id)
-    if index == None:
+
+    # Method 2: Using POSTGRES QL
+    cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
+    deleted_post = cursor.fetchone()
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Post with id {id} does not exist")
-    my_posts.pop(index)
+    
+    # my_posts.pop(index)
+    conn.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id: int, post:Post):
-    index = find_index_post(id)
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail=f"Post with id {id} does not exist")
-    
+    # Method 1: using local data
+    # index = find_index_post(id)
     # convert user input from front end to a dict
     # assign request id to user indut
     # update dict at current index with the new data
-    post_dict = post.model_dump()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    print(post)
-    return {'data': my_posts}
+    # post_dict = post.model_dump()
+    # post_dict['id'] = id
+    # my_posts[index] = post_dict
+    # print(post)
+
+    # Method 2: Using PostGres ql
+    cursor.execute(""" UPDATE posts SET title = %s, content = %s WHERE id = %s RETURNING * """, 
+                   (post.title, post.content, str(id)))
+    updated_post = cursor.fetchone()
+    if updated_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Post with id {id} does not exist")
+    
+    conn.commit()
+    
+    return {'data': updated_post}
