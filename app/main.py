ffbb2562835dff30e7ff21fc539b4ea2ed10769a
pere49 <perekevin49@gmail.com>
2024-05-19 15:25:17 +0300

    hashing passwords using passlib, created files for individual routes

--- app/main.py
+++ app/main.py
@@ -3,21 +3,21 @@ from dotenv import load_dotenv
 
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
-from typing import Optional
+from typing import Optional, List
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
 
-from . import models
-from .database import engine, get_db
-from .schemas import Post
+
+from . import models, database, schemas, utils
 
 load_dotenv()
 
+
 # creating database tables
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=database.engine)
 
 # fastapi instance
 app = FastAPI()
@@ -68,16 +68,16 @@ def find_index_post(id):
 
 """ API Routes """
 @app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
+def test_posts(db: Session = Depends(database.get_db)):
     posts = db.query(models.Posts).all()
-    return {"status": posts}
+    return posts
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/posts")
-def get_posts(db: Session = Depends(get_db)):
+@app.get("/posts", response_model=List[schemas.ResponsePost])
+def get_posts(db: Session = Depends(database.get_db)):
     # Method 2: Using RAW SQL
     # cursor.execute(""" SELECT * FROM posts; """)
     # posts = cursor.fetchall()
@@ -85,10 +85,10 @@ def get_posts(db: Session = Depends(get_db)):
 
     # Method 3: Using SQLAlchemy ORM
     posts = db.query(models.Posts).all()
-    return {"data": posts}
+    return posts
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.ResponsePost)
+def create_posts(post: schemas.Post, db: Session = Depends(database.get_db)):
     # Method 1 using local data
     # post_dict = post.model_dump()
     # post_dict["id"] = randrange(0, 1000000)
@@ -105,11 +105,10 @@ def create_posts(post: Post, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
-    return {"data": new_post}
+    return new_post
 
-
-@app.get("/posts/{id}")
-def get_post(id: int, response: Response, db: Session = Depends(get_db)):
+@app.get("/posts/{id}", response_model=schemas.ResponsePost)
+def get_post(id: int, response: Response, db: Session = Depends(database.get_db)):
     # Method 1: using local data
     # post = find_post(id)
 
@@ -127,11 +126,10 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db)):
         # method 2
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Post with id {id} was not found")
-    return {"post-detail": post}
-
+    return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(database.get_db)):
     # Method 1: Using local data
     # index = find_index_post(id)
     # # my_posts.pop(index)
@@ -152,8 +150,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post:Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.ResponsePost)
+def update_post(id: int, updated_post:schemas.Post, db: Session = Depends(database.get_db)):
     # Method 1: using local data
     # index = find_index_post(id)
     # convert user input from front end to a dict
@@ -179,4 +177,23 @@ def update_post(id: int, updated_post:Post, db: Session = Depends(get_db)):
     
     post_query.update(updated_post.model_dump())
     db.commit()
-    return {'data': post_query.first()}
+    return post_query.first()
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.ResponseUser)
+def create_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    # hash the password from user.password
+    user.password = utils.hash_password(user.password)
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@app.get("/users/{id}", response_model=schemas.ResponseUser)
+def get_user(id: int, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"user with id {id} not found")
+    return user
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 # what we expect from the user
 # title str, content str
@@ -6,3 +7,19 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+
+class ResponsePost(Post):
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class ResponseUser(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes= True
