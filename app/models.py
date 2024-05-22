b7a9f4dde657feee2fd6183ecd8bf7a1681df83f
pere49 <perekevin49@gmail.com>
2024-05-22 22:38:53 +0300

    using Foreign key in the database model to associate a post with a user

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Posts(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE', nullable=False)
