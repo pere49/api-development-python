d438a552d0f285d875d8400728974188d0b2525a
pere49 <perekevin49@gmail.com>
2024-05-30 14:55:36 +0300

    setting up CORS to allow api access from different domains, set to all for now but should have a strict list of allowed domains for security purposes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from . import models, database
 from .routers import post, user, auth, vote
 
 
@@ -9,6 +9,16 @@ from .routers import post, user, auth, vote
 
 # fastapi instance
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
