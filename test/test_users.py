50240b5c6326e9833a0f2f0394817e94d9d60fbe
pere49 <perekevin49@gmail.com>
2024-06-03 19:06:59 +0300

    adjusting the fixture of the tests to module, which is considered as bad practice as tests are dependent on each other, failure of one tests means all tests have failed

--- test/test_users.py
+++ test/test_users.py
@@ -1,51 +1,5 @@
-import pytest
-from fastapi.testclient import  TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from app import schemas, models
-from app.main import app
-from app.database import get_db, Base
-from app.config import settings
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base.metadata.create_all(bind=engine)
-
-# Dependency
-# def override_get_db():
-#     db = TestingSessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-# app.dependency_overrides[get_db] = override_get_db
-
-@pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
+from .database import client, session
+from app import schemas
 
 def test_root(client):
     resp = client.get("/")
@@ -55,9 +9,14 @@ def test_root(client):
 
 def test_create_user(client):
     user = {"email": "test123@gmail.com", "password": "password123"}
-    res = client.post("/users", json=user)
+    res = client.post("/users/", json=user)
     
     new_user =  schemas.ResponseUser(**res.json())
     print(new_user.email)
     assert new_user.email == "test123@gmail.com"
     assert res.status_code == 201
+
+def test_login_user(client):
+    user = {"username": "test123@gmail.com", "password": "password123"}
+    res = client.post("/login", data=user)
+    assert res.status_code == 200
