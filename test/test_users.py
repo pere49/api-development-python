a0194154956a2c0be307f0aa14826d92e724a367
pere49 <perekevin49@gmail.com>
2024-06-03 16:37:14 +0300

    test create new user route

--- test/test_users.py
+++ test/test_users.py
@@ -1,8 +1,20 @@
 from fastapi.testclient import  TestClient
 from app.main import app
+from app import schemas
 
 client = TestClient(app)
 
 def test_root():
     resp = client.get("/")
-    print(resp.json())
+    print(resp.json().get("message"))
+    assert resp.json().get("message") == 'Hello World'
+
+
+def test_create_user():
+    user = {"email": "test123@gmail.com", "password": "password123"}
+    res = client.post("/users", json=user)
+    
+    new_user =  schemas.ResponseUser(**res.json())
+    print(new_user.email)
+    assert new_user.email == "test123@gmail.com"
+    assert res.status_code == 201
