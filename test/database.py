d60b187790c55a132f95030f8209ff04486a7dc0
pere49 <perekevin49@gmail.com>
2024-06-04 09:28:09 +0300

    test login user token generated and verify if token is valid

--- test/database.py
+++ test/database.py
@@ -25,7 +25,7 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 
 # app.dependency_overrides[get_db] = override_get_db
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -36,7 +36,7 @@ def session():
         db.close()
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def client(session):
     def override_get_db():
         try:
--- test/test_users.py
+++ test/test_users.py
@@ -1,5 +1,19 @@
+import pytest
+from jose import jwt
+
 from .database import client, session
 from app import schemas
+from app.config import settings
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "test123@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    user = res.json()
+    user['password'] = "password123"
+    yield user
 
 def test_root(client):
     resp = client.get("/")
@@ -16,7 +30,12 @@ def test_create_user(client):
     assert new_user.email == "test123@gmail.com"
     assert res.status_code == 201
 
-def test_login_user(client):
-    user = {"username": "test123@gmail.com", "password": "password123"}
+def test_login_user(client, test_user):
+    user = {"username": test_user["email"], "password": test_user["password"]}
     res = client.post("/login", data=user)
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
